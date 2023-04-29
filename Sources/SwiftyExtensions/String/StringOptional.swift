bdfffe89d4c7fb73f1943bb67888cef7c1d753ba
stecklum <101799038+stecklum@users.noreply.github.com>
2023-04-29 21:11:13 +0200

    Extensionls of optional strings

---
+++ Sources/SwiftyExtensions/String/StringOptional.swift
@@ -0,0 +1,26 @@
+//
+//  StringOptional.swift
+//  
+//
+//  Created by Tom Stecklum on 29.04.23.
+//
+
+import Foundation
+
+extension String? {
+    
+    /// Safely unwraps optional strings
+    /// - Returns: unwrapped string of empty string
+    public func unwrapped() -> String {
+        guard let self else { return "" }
+        return self
+    }
+    
+    /// Checks wether the provides optional String is null or empty
+    /// - Returns: true, if the string is empty or null, false if the string contains any characters
+    public func isNullOrEmpty() -> Bool {
+        guard unwrapped().isEmpty else { return false }
+        return true
+    }
+    
+}
